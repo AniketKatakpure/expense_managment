11fbdf84e54f035cdf7e9ef09b5432ab0e0a4790
aniket.katakpure <aniket.katakpure@orangebitsindia.com>
2022-01-21 11:47:04 +0530

    my expense app

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <center>
+            <h1 style={{ color: '#c0392b', textShadow: '0 1 3 rgba(0, 0, 0, 0.12), 0 1 2 rgba(0, 0, 0, 0.24)' }}>
+                MyExpense
+            </h1>
+        </center>
+    )
+}
